7c5ed4c0a2762a27e032d215f37b456694f87789
euphoria-anyone <62745373+euphoria-anyone@users.noreply.github.com>
2020-04-14 14:13:02 +0700

    Add files via upload

--- game/game/Program.cs
+++ game/game/Program.cs
@@ -3,136 +3,224 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace game
 {
+    public enum Direction
+    {
+        Stop = 0, Left, Right, Up, Down
+    };
+
     class Program
     {
+        static int[] tailX = new int[360];
+        static int[] tailY = new int[360];
+        static int ntail = 1;
         static public bool gameover;
-        static public int width = 10;
-        static public int height = 10;
-        static public int x, y, fruitx, fruity, score;
-        public enum eDirection { Stop, Left, Right, Up, Down };
-        static public eDirection dir;
-        static eDirection Stop = 0;
+        const int width = 10;
+        const int height = 10;
+        static public int x, y, fruitx, fruity, score;      
+        static public Direction dir;
         
-        static public void setup()//функция настройки нужных параметров
+
+        static public void Setup()//функция настройки нужных параметров
         {
+            Random rand = new Random();
+            int random = rand.Next();
+
             gameover = false;
-            dir = Stop;
-            x = width / 2 - 1;
-            y = height / 2 - 1;
-            var rand = new Random();           
-            fruitx = rand.Next() % width;
-            fruity = rand.Next() % height;
+            dir = Direction.Stop;
+            x = width / 2;
+            y = height / 2;           
+            fruitx = random % (width - 2);
+            fruity = random % (height - 2);
             score = 0;
         }
 
-        static public void draw()//функция прорисовки поля игры
+        static public void Draw()//функция прорисовки поля игры
         {
             Console.Clear();
 
-            for(int i = 0; i < width; ++i)
+            for (int i = 0; i < width; ++i)
             {
-                Console.Write(" #");               
+                Console.Write(" #");
             }
-            Console.Write(" \n");
+            Console.WriteLine(); 
 
             for (int i = 0; i < height; ++i)
             {
-                for(int j = 0; j < width; ++j)
+                for (int j = 0; j < width; ++j)
                 {
-                    if(j == 0 || j == width)
+                    if (j == 0 || j == (width - 1))
                     {
-                        Console.Write("#");
-                        Console.Write("                   ");
-                        Console.Write("#");
-                        if(i == y && j == x)
+                        Console.WriteLine("#");
+                    }
+                    if (i == y && j == x)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.Write("o");
+                    }
+                    else if (i == fruity && j == fruitx)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write("@");
+                    }
+                    else
+                    {
+                        bool print = false;
+                        for (int k = 1; k < ntail; ++k)
                         {
-                            Console.WriteLine("0");
+                            if (tailX[k] == j && tailY[k] == i)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Yellow;
+                                Console.Write("o");
+                                print = true;
+                            }
                         }
-                        else if(i == fruity && j == fruitx)
+                        if (!print)
                         {
-                            Console.WriteLine("f");
+                            Console.Write(" ");
                         }
                     }
-                    Console.Write(" ");                                     
                 }
-                Console.Write("\n");
+                Console.WriteLine();
             }
 
-            for (int i = 0; i < width; ++i)
+            for(int i = 0; i <= width; ++i)
             {
-                Console.Write(" #");                
+                Console.Write("#");
             }
-            Console.Write(" ");
+            Console.WriteLine();
             Console.WriteLine("\nscore: " + score);
-            Console.ReadLine();
         }
-        static public void input()//функция отслежки нажатий от игрока 
+
+        static public void Input()//функция отслежки нажатий от игрока 
         {
-            ConsoleKey tap = Console.ReadKey(true).Key;
-            switch(tap)
+            if (Console.KeyAvailable)
             {
-                case ConsoleKey.UpArrow:
-                    dir = eDirection.Up;
-                    break;
-                case ConsoleKey.DownArrow:
-                    dir = eDirection.Down;
-                    break;
-                case ConsoleKey.LeftArrow:
-                    dir = eDirection.Left;
-                    break;
-                case ConsoleKey.RightArrow:
-                    dir = eDirection.Right;
-                    break;
-                case ConsoleKey.Escape:
-                    gameover = true;
-                    break;
+                ConsoleKey keyInfo = Console.ReadKey(true).Key;
+                switch (keyInfo)
+                {
+                    case ConsoleKey.UpArrow:
+                        dir = Direction.Up;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        dir = Direction.Down;
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        dir = Direction.Left;
+                        break;
+                    case ConsoleKey.RightArrow:
+                        dir = Direction.Right;
+                        break;
+                    case ConsoleKey.Escape:
+                        gameover = true;
+                        break;
+                }
+            }     
+        }
+        static public void Logic()//функция логики игры
+        {
+            tailX[0] = x;
+            tailY[0] = y;
+            int prevx = tailX[0];
+            int prevy = tailY[0];
+            int prev2X, prev2Y;
+            for(int i = 0; i < ntail; ++i)
+            {
+                prev2X = tailX[i];
+                prev2Y = tailY[i];
+                tailX[i] = prevx;
+                tailY[i] = prevy;
+                prevx = prev2X;
+                prevy = prev2Y;
             }
-        }
-        static public void logic()//функция логики игры
-        {
-            switch(dir)
+
+            switch (dir)
             {
-                case eDirection.Stop:
+                case Direction.Stop:
                     dir = 0;
                     break;
-                case eDirection.Up:
-                    y -= 1;
+                case Direction.Up:
+                    y--;
                     break;
-                case eDirection.Down:
-                    y += 1;
+                case Direction.Down:
+                    y++;
                     break;
-                case eDirection.Left:
-                    x -= 1;
+                case Direction.Left:
+                    x--;
                     break;
-                case eDirection.Right:
-                    x += 1;
-                    break;            
+                case Direction.Right:
+                    x++;
+                    break;
             }
-            if(x > width || x < 0 || y > height || y < 0)
+            if (x > width || x < 0 || y > height || y < 0)
             {
                 gameover = true;
             }
-            if(x == fruitx && y == fruity)
+            if (x == fruitx && y == fruity)
             {
-                score += 1;
-                var rand = new Random();
-                fruitx = rand.Next() % width;
-                fruity = rand.Next() % height;                
+                score += 10;
+                Random rand = new Random();
+                int random = rand.Next();
+                fruitx = random % width;
+                fruity = random % height;
             }
         }
         static public void Main(string[] args)
         {
-            setup();
-            while(!gameover)
+            int n;
+            int Easy = 1;
+            int Medium = 2;
+            int Hard = 3;
+            int Exit = 4;
+            Console.WriteLine("Select level(1 - Easy, 2 - Medium, 3 - Hard, 4 - Exit): ");
+            n = Int32.Parse(Console.ReadLine());
+            Console.Clear();
+            if (n == Easy)
             {
-                draw();
-                input();
-                logic();
+                Setup();
+                while (!gameover)
+                {
+                    Draw();
+                    Input();
+                    Logic();
+                }
+            }
+            else if (n == Medium)
+            {
+
+                Setup();
+                while (!gameover)
+                {
+                    Draw();
+                    Input();
+                    Logic();
+                }
+            }
+            else if (n == Hard)
+            {
+
+                Setup();
+                while (!gameover)
+                {
+                    Draw();
+                    Input();
+                    Logic();
+                }
+            }
+            else if(n == Exit)
+            {
+                gameover = true;
+            }
+            else
+            {
+                Console.WriteLine("Restart game and select correct level(Easy, Medium, Hard)!");
             }
             Console.ReadKey();
+
         }
     }
 }
+
